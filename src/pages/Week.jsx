3912335058d288b82acecbc375fad9c07dfe76e5
Edgar Guerrero <edgarguerrero37@gmail.com>
2022-04-25 18:21:22 -0500

    Configuracion de las paginas por tipo de reporte

--- src/pages/Week.jsx
+++ src/pages/Week.jsx
@@ -1,11 +1,66 @@
 import Header from '../shared/Header';
+import React, { useState, useEffect } from "react";
+import { api } from '../utils/api';
+import { Container, Col, Button, Tab, Row, Table } from 'react-bootstrap';
 
 function Week(){
-    return(
-        <>
-            <Header/>
-        </>
-    )
+
+    var url = "http://localhost/downloadSemanal.php?file=";
+
+    const [ archivos, setArchivos ] = useState([]);
+
+    useEffect(() => {
+        //con esta funcion pedimos todos los datos a la api
+        const fetchData = async () => {
+            const traerDatos = await api.files.Semanal();
+            setArchivos(Object.values(traerDatos));
+            //console.log(Object.values(traerDatos));
+        };
+
+        fetchData();
+
+        }, []);
+
+    if (!archivos.length) {
+        return (
+            <>
+                <Header />
+                <h1 class="title">No se registran Informes!</h1>
+            </>
+        );
+        } 
+    else {
+        return(
+            <>
+                <Header/>
+                    <Container className="secondary">
+                    <Col>
+                        <Button variant="secondary" className="border border-5 border-white" href="/">Volver</Button>{ ' ' }
+                        <Tab.Container defaultActiveKey="first" className="border border-5 Table" >
+                            <Row className="border border-5 border-white">
+                                <Col md="auto" style={{padding: '5rem'}, {width: '70rem'}}>
+                                    <Table bordered hover size="sm" responsive="xl">
+                                        <thead>
+                                            <tr>
+                                                <th colSpan="2">Informes Semanales</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody >
+                                        { archivos.map((value) => (
+                                            <tr>
+                                                <a href={url+value} download>{Object.values(value)}</a>
+                                            </tr>
+                                        )) }
+                                        </tbody>
+                                    </Table>
+                                </Col>
+                            </Row>
+                        </Tab.Container>
+                    </Col>
+                    </Container>
+            </>
+        )
+    }
 }
 
 export default Week;
---
+++ src/utils/api.jsx
@@ -0,0 +1,24 @@
+const callApi = async (url, options = {}) => {
+    options.headers = {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    };
+
+    const response = await fetch('http://localhost' + url);
+    const data = await response.json();
+    return data;
+};
+
+export const api = {
+    files: {
+        Diario() {
+            return callApi('/Diario.php');
+        },
+        Semanal() {
+            return callApi('/Semanal.php');
+        },  
+        Mensual() {
+            return callApi('/Mensual.php');
+        },       
+    }
+};
